00abc9fd77ab180b7ef4d4bb67aa11b736875def
jxxl998 <jessielee1998_sc@foxmail.com>
2022-12-03 14:45:50 +0800

    complete templates

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
-/////////////////
-// FILE
+///////////
+// FILE //
+/////////
+
 // blocking, synchronous way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 
@@ -25,23 +27,68 @@ const url = require('url');
 
 // console.log('will read file!');
 
-//////////
-// SERVER
+/////////////
+// SERVER //
+///////////
+
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+
+  return output;
+};
 
 // __dirname means the current directory
+const templateOverview = fs.readFileSync(
+  `${__dirname}/templates/overview.html`,
+  'utf-8'
+);
+const templateProduct = fs.readFileSync(
+  `${__dirname}/templates/product.html`,
+  'utf-8'
+);
+const templateCard = fs.readFileSync(
+  `${__dirname}/templates/card.html`,
+  'utf-8'
+);
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-const dataObject = JSON.parse(data);
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
+  // overview page
   if (pathName === '/' || pathName === '/overview') {
-    res.end('This is the OVERVIEW');
+    res.writeHead(200, { 'Content-type': 'text/html' });
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(templateCard, el))
+      .join('');
+
+    const output = templateOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
+
+    // product page
   } else if (pathName === '/product') {
     res.end('This is the PRODUCT');
+
+    // api
   } else if (pathName === '/api') {
+    // read json file and return
     res.writeHead(200, { 'Content-type': 'application/json' });
     res.end(data);
+
+    // not found
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
